6d7296cdb91c544386c0faf328832f8105298c91
jghdev <jghdev@jghdevs-iMac.local>
2018-04-17 04:14:34 -0500

    Create cors enable express server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const fetch = require('node-fetch');
+const port = process.env.PORT || 3000;
+app.use(cors())
+
+app.get('/getdata', (req, res) => {
+  fetch('https://search-api.swiftype.com/api/v1/public/engines/search.json?q=asp&page=1&per_page=10&facets%5Bpage%5D%5B%5D=product&facets%5Bpage%5D%5B%5D=website_section&facets%5Bpage%5D%5B%5D=documentation_type&engine_key=5jZG1gmmCTFYbSSDjpqq')
+    .then(resp => resp.json())
+    .then(json => res.send(json));
+  // res.send('success');
+})
+
+app.listen(port, () => console.log('Example app listening on port 3000!'))
