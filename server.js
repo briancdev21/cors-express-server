c4def02287bda81a2bd4a97d66deaff4af97a536
jghdev <jghdev@jghdevs-iMac.local>
2018-04-17 07:52:46 -0500

    update server

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 
 app.get('/getdata', (req, res) => {
-  fetch('https://search-api.swiftype.com/api/v1/public/engines/search.json?q=asp&page=1&per_page=10&facets%5Bpage%5D%5B%5D=product&facets%5Bpage%5D%5B%5D=website_section&facets%5Bpage%5D%5B%5D=documentation_type&engine_key=5jZG1gmmCTFYbSSDjpqq')
+  const query = req.query.searchname;
+  fetch(`https://search-api.swiftype.com/api/v1/public/engines/search.json?q=${query}&page=1&per_page=10&facets%5Bpage%5D%5B%5D=product&facets%5Bpage%5D%5B%5D=website_section&facets%5Bpage%5D%5B%5D=documentation_type&engine_key=5jZG1gmmCTFYbSSDjpqq`)
     .then(resp => resp.json())
     .then(json => res.send(json));
   // res.send('success');
